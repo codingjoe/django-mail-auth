5ba1d0d0e30d89338a5ce923bba23a8adcee124c
Johannes Maron <johannes@maron.family>
2021-10-20 13:36:11 +0200

    Fix next url encoding error
    Next URLs where not properly encoded and could pass the wrong information

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -11,6 +11,22 @@ class TestBaseLoginForm:
 
 
 class TestEmailLoginForm:
+    def test_get_login_url(self, rf):
+        request = rf.get("/")
+        form = EmailLoginForm(request=request)
+        assert (
+            form.get_login_url(request, "TOKEN")
+            == "http://testserver/accounts/login/TOKEN"
+        )
+        assert (
+            form.get_login_url(
+                request,
+                "TOKEN",
+                next="/path/?utm_source=website&utm_medium=email#some-anchor",
+            )
+            == "http://testserver/accounts/login/TOKEN?next=/path/%3Futm_source%3Dwebsite%26utm_medium%3Demail%23some-anchor"
+        )
+
     def test_send_mail__html_template(self):
         class MyEmailLoginForm(EmailLoginForm):
             html_email_template_name = EmailLoginForm.email_template_name
